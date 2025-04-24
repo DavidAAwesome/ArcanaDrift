588714504f27a2bd379fd9ed00650e2d7645e71f
David Aduku <130676739+DavidAAwesome@users.noreply.github.com>
2025-04-24 13:35:30 -0400

    fixed moving mouse in end screen

--- Arcana Drift/Assets/Scripts/GameEndManager.cs
+++ Arcana Drift/Assets/Scripts/GameEndManager.cs
@@ -3,6 +3,11 @@ using UnityEngine.SceneManagement;
 
 public class GameEndManager : MonoBehaviour
 {
+    public void Start()
+    {
+        Cursor.lockState = CursorLockMode.None;
+        Cursor.visible = true;
+    }
     public void restart()
     {
         SceneManager.LoadScene("RuinedArchives");
